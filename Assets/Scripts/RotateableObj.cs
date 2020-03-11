a4c83357cdda7881522b8498d4a06aa94da0df4e
Floyd Lehman <flehman2000@gmail.com>
2020-03-11 13:10:55 +0100

    Rewrited the code and did some name fixes of the objects and materials and code names

---
+++ Assets/Scripts/RotateableObj.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateableObj : MonoBehaviour
+{
+    //<summary>
+    //This script has to be placed on the object that you want to rotate with touch input.
+    //<summary>
+
+    [SerializeField]
+    private float rotationSpeed = 0.5f;
+
+    private void Update()
+    {
+        // get the user touch input
+        foreach (Touch touch in Input.touches)
+        {
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+
+                transform.Rotate(touch.deltaPosition.y * rotationSpeed, -touch.deltaPosition.x * rotationSpeed, 0, Space.World);
+
+            }
+        }
+    }
+
+}
